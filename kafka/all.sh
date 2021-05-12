7b56b45c78bd0c20b7a88bb4b4ffd388aebd36b1
Francis Stephens <francisstephens@gmail.com>
2021-05-12 15:42:21 +0200

    Update kafka download url

--- kafka/all.sh
+++ kafka/all.sh
@@ -7,7 +7,7 @@ sudo apt-get --assume-yes install kafkacat
 mkdir ~/kafka
 cp ./*.sh ~/kafka
 pushd ~/kafka
-curl "http://www-eu.apache.org/dist/kafka/1.1.0/kafka_2.12-1.1.0.tgz" -o ~/kafka/kafka.tgz
+curl "http://archive.apache.org/dist/kafka/1.1.0/kafka_2.12-1.1.0.tgz" -o ~/kafka/kafka.tgz
 tar -xvzf ~/kafka/kafka.tgz --strip 1
 
 popd
